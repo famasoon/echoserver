5925bcc55aa3cfb0c3399de6ab08c68e1ed5ca7b
FAMASoon <famasoon@gmail.com>
2023-01-08 03:08:15 +0000

    fix typo

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use std::{env, str, thread};
 fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
     let addr = &args[1];
-    echo_server(addr);
+    echo_server(addr)?;
     Ok(())
 }
 
